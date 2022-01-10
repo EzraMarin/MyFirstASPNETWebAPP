02c36602805e1f4f0dbf31cdbd0bccddbb2bc0ca
EzraMarin <90418312+EzraMarin@users.noreply.github.com>
2022-01-09 20:29:31 -0700

    Add project files.

---
+++ MyFirstASPNETWebAPP/wwwroot/NewFolder/fanmail.js
@@ -0,0 +1,5 @@
+﻿document.getElementById("btnSend").addEventListener("click", function () {
+    alert("from: " + document.getElementById("txtfrom").value +
+        ", subject: " + document.getElementById("txtsubject").value +
+        ", message: " + document.getElementById("txtmessage").value);
+})
